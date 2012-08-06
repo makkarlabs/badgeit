00c28863d124d3b6393efe1f12c15c184fe67e1f
MakkarLabs <admin@makkarlabs.in>
2012-08-06 07:58:09 -0500

    First commit

---
+++ resources/js/fonts.js
@@ -0,0 +1,16 @@
+var fonts = new Array(
+	'Arial,Arial,Helvetica,sans-serif',
+	'Arial Black,Arial Black,Gadget,sans-serif',
+	'Comic Sans MS,Comic Sans MS,cursive',
+	'Courier New,Courier New,Courier,monospace',
+	'Georgia,Georgia,serif',
+	'Impact,Charcoal,sans-serif',
+	'Lucida Console,Monaco,monospace',
+	'Lucida Sans Unicode,Lucida Grande,sans-serif',
+	'Palatino Linotype,Book Antiqua,Palatino,serif',
+	'Tahoma,Geneva,sans-serif',	
+	'Times New Roman,Times,serif',
+	'Trebuchet MS,Helvetica,sans-serif',
+	'Verdana,Geneva,sans-serif',
+	'Calibri' );
+var fontstyle = new Array('Normal','Bold','Italic');	
