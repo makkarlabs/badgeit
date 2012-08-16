76f2fb853a048c332c219c942484e6bc0484afa4
Madhumitha Viswanathan <madhuvishy@gmail.com>
2012-08-16 18:29:32 +0530

    Google Drive image chooser works

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,17 +1,12 @@
-$.ready(function(){
-	$('#authnstart').on('click',function(e){
-		e.preventDefault();
-		auth();
-		location.href="./start.html";
-	});
-});
 function auth() 
 {
         var config = {
           'client_id': '434888942442.apps.googleusercontent.com',
-          'scope': 'https://www.googleapis.com/auth/drive'
+          'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive'
         };
         gapi.auth.authorize(config, function() {
           	localStorage['accesstoken'] = gapi.auth.getToken().access_token;
+		location.href="./start.html";
         });
+
 }
